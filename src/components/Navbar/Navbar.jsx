c782cc79bac03372326c6f0a3568931710ea332b
misikira <misikirteka@gmail.com>
2023-10-10 10:19:43 +0300

    add login and book table menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,6 +26,14 @@ const Navbar = () => (
 				<a href="contact">Contact</a>
 			</li>
 		</ul>
+		<div className="app__navbar-login">
+			<a href="#login" className="p__opensans">
+				Log In /Register
+			</a>
+			<a href="#" className="p__opensans">
+				Book Table
+			</a>
+		</div>
 	</nav>
 );
 
