116fcd873ab078dd2cf0b07b04e24a8dd6e8cbf0
misikira <misikirteka@gmail.com>
2023-10-10 09:54:11 +0300

    Add navbar menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-
-import './Navbar.css';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+	<nav className="app__navbar">
+		<div className="app__navbar-logo">
+			<img src={images.logo} alt="reataurant logo" />
+		</div>
+		<ul className="app__navbar-links">
+			<li className="p__opensans">
+				<a href="home">Home</a>
+			</li>
+			<li className="p__opensans">
+				<a href="about">About</a>
+			</li>
+			<li className="p__opensans">
+				<a href="menu">Menu</a>
+			</li>
+			<li className="p__opensans">
+				<a href="awards">Awards</a>
+			</li>
+			<li className="p__opensans">
+				<a href="contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
 );
 
 export default Navbar;
