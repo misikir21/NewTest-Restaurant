6ce611421958999be85417f4e48066d6cd9ea496
misikira <misikirteka@gmail.com>
2023-10-10 15:23:15 +0300

    add explore menu button

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 import "./Header.css";
 
@@ -8,7 +9,14 @@ const Header = () => (
 		<div className="app__wrapper_info">
 			<SubHeading tittle={"chase the new flavor"} />
 			<h1 className="app__header-h1">the key to fine dining</h1>
-			<p className="p__opensans" style={{ margin: "2rem 0" }}></p>
+			<p className="p__opensans" style={{ margin: "2rem 0" }}>
+				Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+				morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+				tellus{" "}
+			</p>
+			<button type="button" className="custom__button">
+				Explore Menu
+			</button>
 		</div>
 		<div className="app_wrapper_img"></div>
 	</div>
