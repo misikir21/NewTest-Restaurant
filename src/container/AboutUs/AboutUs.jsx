40b28cc4bab1746d70a520dbc8cf28642b1d271a
misikira <misikirteka@gmail.com>
2023-10-10 16:14:45 +0300

    Apply the final touches of the about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -10,6 +10,36 @@ const AboutUs = () => (
 		<div className="app__aboutus-overlay flex__center">
 			<img src={images.G} alt="g letter" />
 		</div>
+		<div className="app__aboutus-content flex__center">
+			<div className="app__aboutus-content-about">
+				<h1 className="headtext__cormorant">About Us</h1>
+				<img src={images.spoon} alt="spoon" className="sppon__img" />
+				<p className="p__opensans">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni,
+					magnam.Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Magni, magnam
+				</p>
+				<button type="button" className="custom__button">
+					Know More
+				</button>
+			</div>
+			<div className="app__aboutus-content_knife flex__center">
+				<img src={images.knife} alt="about knife" />
+			</div>
+
+			<div className="app__aboutus-content-history">
+				<h1 className="headtext__cormorant">History</h1>
+				<img src={images.spoon} alt="spoon" className="sppon__img" />
+				<p className="p__opensans">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni,
+					magnam.Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Magni, magnam
+				</p>
+				<button type="button" className="custom__button">
+					Know More
+				</button>
+			</div>
+		</div>
 	</div>
 );
 
