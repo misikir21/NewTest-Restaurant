08ee13668c9b62a440ff5357182c3080a5b6dc70
misikira <misikirteka@gmail.com>
2023-10-10 14:18:52 +0300

    add inline style

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => (
 		<div className="app__wrapper_info">
 			<SubHeading tittle={"chase the new flavor"} />
 			<h1 className="app__header-h1">the key to fine dining</h1>
+			<p className="p__opensans" style={{ margin: "2rem 0" }}></p>
 		</div>
 		<div className="app_wrapper_img"></div>
 	</div>
