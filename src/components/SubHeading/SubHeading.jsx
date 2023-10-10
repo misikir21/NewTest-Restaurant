1fd2f695de22a64fb8445bab814d3e0accc944dd
misikira <misikirteka@gmail.com>
2023-10-10 14:13:47 +0300

    add the header compo

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
+const SubHeading = ({ tittle }) => (
+	<div style={{ marginBottom: "1rem" }}>
+		<p className="p__cormorant">{tittle}</p>
+		<img src={images.spoon} alt="spoon" className="spoon__img" />
+	</div>
 );
 
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+	<div className="app__header app__wrapper section__padding" id="home">
+		<div className="app__wrapper_info">
+			<SubHeading tittle={"chase the new flavor"} />
+			<h1 className="app__header-h1">the key to fine dining</h1>
+		</div>
+		<div className="app_wrapper_img"></div>
+	</div>
 );
 
 export default Header;
