5f91b2a03300a3ce7d196939a0ce79149f7bf534
misikira <misikirteka@gmail.com>
2023-10-11 11:00:09 +0300

    add more style and content for the special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -16,7 +16,12 @@ const SpecialMenu = () => (
 				<p className="app__specialMenu-menu_heading">Wine & Beer</p>
 				<div className="app__specialMenu_menu_items">
 					{data.wines.map((wine, index) => (
-						<p>{wine.title}</p>
+						<MenuItem
+							key={wine.title + index}
+							title={wine.title}
+							price={wine.price}
+							tags={wine.tags}
+						/>
 					))}
 				</div>
 			</div>
