a25ba37818701d9d759fac55926be3c7226de25f
misikir21 <tekamisikir@gmail.com>
2023-10-13 11:33:43 -0700

    add the larvels feature

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {SubHeading} from '../../components'
+import { images,data } from '../../constants';
 
 import './Laurels.css';
+const AwardCard=({award:{imgUrl,title,subtitle}})=>
+  <div className="app__laurels_awards-cards "> 
+   <img src={imgUrl} alt="award image" />
+   <div className='app__laurels_awards-card_content'>
+    <p className='p__cormorant' style={{color:"#dcca88"}}>{title}</p>
+    <p className='p__cormorant'>{subtitle}</p>
+   </div>
+  </div> 
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding'> 
+  <div className='app__wrapper_info'>
+
+  </div>
+    <div className='app__wrapper_img'>
+    <SubHeading title="Awards & recognition" />
+    <h1 className='headtext__cormorant'>Our Laurels</h1>
+    <div className='app__laurels_awards'>
+      {data.awards.map((award)=><AwardCard award={award} key={award.title}/>)}
+    </div>
+
+    </div>
   </div>
 );
 
