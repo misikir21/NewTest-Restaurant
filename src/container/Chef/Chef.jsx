b695de22135a174decd28dea2550f29177d5f409
misikir21 <tekamisikir@gmail.com>
2023-10-13 08:50:50 -0700

    add the chef sectio

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {subHeading} from '../../components'
+import {images} from'../../constants'
+import './Chef.css'
+
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app_bg app__wrapper section__paddjng'>
+    <div className='app__wrapper_ig app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+
+    </div>
   </div>
 );
 
