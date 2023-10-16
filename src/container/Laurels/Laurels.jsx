032e499bf7fd79b3436a7d355d15a68d5cfd9a31
misikir21 <tekamisikir@gmail.com>
2023-10-16 00:10:05 -0700

    apply the finishing touches of the laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import {SubHeading} from '../../components'
-import { images,data } from '../../constants';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
-const AwardCard=({award:{imgUrl,title,subtitle}})=>
-  <div className="app__laurels_awards-cards "> 
-   <img src={imgUrl} alt="award image" />
-   <div className='app__laurels_awards-card_content'>
-    <p className='p__cormorant' style={{color:"#dcca88"}}>{title}</p>
-    <p className='p__cormorant'>{subtitle}</p>
-   </div>
-  </div> 
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
 
 const Laurels = () => (
-  <div className='app__bg app__wrapper section__padding'> 
-  <div className='app__wrapper_info'>
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
 
-  </div>
-    <div className='app__wrapper_img'>
-    <SubHeading title="Awards & recognition" />
-    <h1 className='headtext__cormorant'>Our Laurels</h1>
-    <div className='app__laurels_awards'>
-      {data.awards.map((award)=><AwardCard award={award} key={award.title}/>)}
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
     </div>
 
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
     </div>
   </div>
 );
