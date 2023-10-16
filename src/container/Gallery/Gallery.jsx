f1377bbd9c48f7d161a93b416cd92ec3315562ba
misikir21 <tekamisikir@gmail.com>
2023-10-16 05:33:57 -0700

    add the gallery with some styles

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import {BsInstagram,BsArrowLeftShort,BsArrowRightShort} from 'react-icons/bs';
+import {SubHeading} from '../../components'
+import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
+
+const Gallery = () => {
+return ( 
+   <div className='app__gallery flex__center'>
+    <div className='app__gallery-content'>
+      <SubHeading title={"Instagram"}/>
+
+    </div>
     Gallery
   </div>
 );
-
+}
 export default Gallery;
