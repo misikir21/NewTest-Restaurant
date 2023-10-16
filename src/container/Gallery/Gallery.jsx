9361c74a4330a6778c63bf5875d4d5dee472462c
misikir21 <tekamisikir@gmail.com>
2023-10-16 05:44:43 -0700

    complete the jsx compoet

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -6,13 +6,18 @@ import './Gallery.css';
 
 
 const Gallery = () => {
+  const scrollref=React.useRef(null)
 return ( 
    <div className='app__gallery flex__center'>
     <div className='app__gallery-content'>
       <SubHeading title={"Instagram"}/>
-
-    </div>
-    Gallery
+     <h1 className='headtext-cormorant'>Photo</h1>
+     <p className='p__opensand' style={{color:"aaa" ,marginTop:"2rem"}}></p>
+     <button className='custom__button' type='button'>View More</button> </div>
+     <div className='app__gallery-images'>
+      <div className='app__gallery_images_contianer' ref={scrollref}></div>
+     </div>
+  
   </div>
 );
 }
