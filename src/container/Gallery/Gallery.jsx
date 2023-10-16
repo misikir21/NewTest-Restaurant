f6613893894bcef35260933090dbd5aafc20fbae
misikir21 <tekamisikir@gmail.com>
2023-10-16 08:46:29 -0700

    complete the gallery sectio

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import {BsInstagram,BsArrowLeftShort,BsArrowRightShort} from 'react-icons/bs';
-import {SubHeading} from '../../components'
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
-const Galleryimges=[images.gallery01,images.gallery02,images.gallery03,images.gallery04]
 
 const Gallery = () => {
-  const scrollref=React.useRef(null);
-  const scroll=(direction)=>{
-    const {current}=scrollref;
-    if(direction ==='left'){
-    current.scrolleft -=300;
-  }
-  else {
-    current.scrolleft +=300
-  }
-}
-return ( 
-   <div className='app__gallery flex__center'>
-    <div className='app__gallery-content'>
-      <SubHeading title={"Instagram"}/>
-     <h1 className='headtext-cormorant'>Photo</h1>
-     <p className='p__opensand' style={{color:"aaa" ,marginTop:"2rem"}}></p>
-     <button className='custom__button' type='button'>View More</button> </div>
-     <div className='app__gallery-images'>
-      <div className='app__gallery_images_contianer' ref={scrollref}>
-        (/*loping throug gallery object*/)
-        { Galleryimges.map((images,index)=>(
-         <div className='app__galler-images_card flex__center' key={`Galleryimges-${index + 1}`}>
-         <img src={images} alt="gallery" />
-         <BsInstagram className='gallery__images-icon'/>
-         </div> 
-        ))}
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
-     </div>
-     <div className='app__gallery-images_arrow'>
-      <BsArrowLeftShort className="gallery__arrow-icon" onClick={()=>scroll('left')} />
-      <BsArrowRightShort className="gallery__arrow-icon" onClick={()=>scroll('right')} />
-     </div>
-  
-  </div>
-);
-}
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Gallery;
